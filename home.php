2b185e49183c395e84e8de56fcf93292321c7c79
The-Jackrabbit <lsm5fm@virginia.edu>
2017-09-18 11:28:55 -0400

    Definition Card

--- home.php
+++ home.php
@@ -130,6 +130,8 @@
 				padding: 12pt;
 				display: inline-block;
 				margin: 12pt;
+				vertical-align: top;
+				min-height: 520pt;
 			}
 			.info-icon {
 				width: 100%;
@@ -154,6 +156,43 @@
 			
 			ul.leaf {
 				list-style-image: url('/Assets/leafbullet.png');
+				padding-left: 5pt;
+			}
+			.actions input {
+				color: white;
+				border: none !important;
+				width: 200pt;
+				height: 50pt;
+				margin: 12pt;
+				font-size: 18pt;
+				background-color: #7A1906  !important;
+			}
+			.actions {
+				width: min-content;
+				white-space: nowrap;
+				margin: 0 auto;
+			}
+			.actions * {
+				display: inline-block;
+			}
+			div.definition {
+				background-color: white;
+				padding: 24pt 12pt;
+				width: 86%;
+				margin: 0 auto;
+				box-shadow: 4pt 4pt 4pt grey;
+			}
+			td.enumeration {
+				width: 20pt;
+			}
+			td.definition {
+				line-height: 125%;
+			}
+			div.definition h3 {
+				margin: 6pt 0pt;
+			}
+			table.definition tr {
+				height: 50pt;
 			}
 		</style>
 
@@ -190,6 +229,20 @@
 		</div>
 		<div class="banner-padding"></div>
 		<div class="max-inline">
+			<div class="definition">
+				<h1>Definition of CrunchBag</h1>
+				<h3><span>plural</span> CrunchyBois</h3>
+				<table class="definition">
+					<tr>
+						<td class="enumeration">1</td>
+						<td class="definition">noun: a dope bag of good smelly crunchy ass leaves that you can jump into, spread around your home, eat (though we don't recommend it), or do with whatever your heart desires idk man they're your leaves now </td>
+					</tr>
+					<tr>
+						<td class="enumeration">2</td>
+						<td class="definition">noun: a bag that emulates that sweet sound of crunchy leaves when cronched </td>
+					</tr>
+				</table>
+			</div>
 			<div class="cards">
 
 				<div class="home-info-card">
@@ -222,14 +275,26 @@
 						<img src="./Assets/box-pentagon.png">
 					</div>
 					<div class="info-text">
-						<h1 style= "font-weight: bold; padding: 20px; color: #4f4541; ">Autumn: </br> <span style= "font-weight:normal; font-size: 25px; font-style: italic;">only a few clicks away... </span></h1>
+						<h1 style= "font-weight: bold; padding: 20px; color: #4f4541; ">Autumn</h1>
 						<ul class= "leaf">
 							<li style="margin: 10px; text-indent: -10px;"><p class="card-text">A <span style= "font-family: 'Lora', serif; font-weight: bold;">CrunchBag</span> leaf bag is stuffed with dry, crisp leaves to be sent to your door step at anytime of year.</p> </li>
 							<li style="margin: 10px; text-indent: -10px;"><p class="card-text"> Recieve your <span style= "font-family: 'Lora', serif; font-weight: bold;">CrunchBag</span> bag and enjoy the autumn feels.</p></li>
+							<li style="margin: 10px; text-indent: -10px;"><p class="card-text"> With CrunchBag, autumn is only a few clicks away... </p></li>
+							
 						</ul>		
+
 					</div>
 				</div>
 
+			</div>
+
+			<div class="actions">
+				<form action="./signup.php">
+					<input class="signup" type="submit" value="Get Started">
+				</form>
+				<form action="./purchase.php">
+					<input class="purchase" type="submit" value="Learn More">
+				</form>
 			</div>
 			
 
