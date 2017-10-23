b4eebe151e2e11900a6582fb5f2fb69f3647de02
krp9da <krp9da@virginia.edu>
2017-10-23 14:58:49 -0400

    fix button

--- purchaseConfirmation.php
+++ purchaseConfirmation.php
@@ -73,15 +73,15 @@
 					<label>Total: </label><?php echo "$".number_format($sum, 2);?>
 				</span>
 				<?php
-					$sum = 0.04;
+					$sum = 0.30;
 				?>
 				<form action="https://test.bitpay.com/checkout" method="post" >
-					<input type="hidden" name="action" value="checkout" />
-					<input type="hidden" name="posData" value="" />
+  					<input type="hidden" name="action" value="checkout" />
+  					<input type="hidden" name="posData" value="" />
 					<input type="hidden" name="price" value="<?php echo $sum;?>" />
-					<input type="hidden" name="data" value="gKARxl3aXk+XK4ebKD2NeCzVODQZETKfLsZcHDtlADhaLM3br+dkzveLlDJdGC0marp9lyhuBmZGFQESte1b/9R/LyMXktY2vg9DxezTXE7NcNlr32DomZYURUD/FQqkNI5Ejhfn+L7Gi1wddJSMM9TTzv0g/nSvHzBupOgYWn8OZxPZ1jRP/bexS06mkqAijWuHC8Kpl4qaO9V9aaXIUYcsRsaTMFC4vcWou3U4rwIBLWTQfxkUHpJlyKLHTBVk" />
-					<input type="image" src="https://test.bitpay.com/img/button-large.png" border="0" name="submit" alt="BitPay, the easy way to pay with bitcoins." >
-				 </form>
+  					<input type="hidden" name="data" value="gKARxl3aXk+XK4ebKD2NeCzVODQZETKfLsZcHDtlADhaLM3br+dkzveLlDJdGC0marp9lyhuBmZGFQESte1b/9R/LyMXktY2vg9DxezTXE7NcNlr32DomZYURUD/FQqkNI5Ejhfn+L7Gi1wddJSMM9TTzv0g/nSvHzBupOgYWn8OZxPZ1jRP/bexS06mkqAijWuHC8Kpl4qaO9V9aaXIUYcsRsaTMFC4vcWou3U4rwIBLWTQfxkUHpJlyKLHTBVk" />
+  					<input type="image" src="https://test.bitpay.com/img/button-large.png" border="0" name="submit" alt="BitPay, the easy way to pay with bitcoins." >
+				</form>
 				
 				
 			</div>
