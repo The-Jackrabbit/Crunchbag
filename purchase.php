b21630a6c2f5989f7cd09e0ee7eecf75ccf7e76f
The-Jackrabbit <lsm5fm@virginia.edu>
2017-10-23 13:46:17 -0400

    Bitcoin button integration

--- purchase.php
+++ purchase.php
@@ -177,13 +177,7 @@
 		</form>
 		
 	</div>
-<form action="https://test.bitpay.com/checkout" method="post" >
-  <input type="hidden" name="action" value="checkout" />
-  <input type="hidden" name="posData" value="" />
-  <input type="hidden" name="price" value="2" />
-  <input type="hidden" name="data" value="gKARxl3aXk+XK4ebKD2NeCzVODQZETKfLsZcHDtlADhaLM3br+dkzveLlDJdGC0marp9lyhuBmZGFQESte1b/9R/LyMXktY2vg9DxezTXE7NcNlr32DomZYURUD/FQqkNI5Ejhfn+L7Gi1wddJSMM9TTzv0g/nSvHzBupOgYWn8OZxPZ1jRP/bexS06mkqAijWuHC8Kpl4qaO9V9aaXIUYcsRsaTMFC4vcWou3U4rwIBLWTQfxkUHpJlyKLHTBVk" />
-  <input type="image" src="https://test.bitpay.com/img/button-large.png" border="0" name="submit" alt="BitPay, the easy way to pay with bitcoins." >
-</form>
+
 
 	<script>
 		$("button").click(function() {
--- purchaseConfirmation.php
+++ purchaseConfirmation.php
@@ -72,13 +72,16 @@
 				<span class="cart-total">
 					<label>Total: </label><?php echo "$".number_format($sum, 2);?>
 				</span>
-				<form action="./bitcoinpay.php" method="POST">
-					<input type="submit" value="Bitcoin buy">
-					<input type="hidden" name="amount" value="<?php echo $sum;?>">
-					<input type="hidden" name="customer" value="<?php echo $_SESSION["email"];?>">
-					<input type="hidden" name="merchant" value="<?php echo "crunchbagOfficial@gmail.com";?>">
-					
-				</form>
+				<?php
+					$sum = 0.04;
+				?>
+				<form action="https://test.bitpay.com/checkout" method="post" >
+					<input type="hidden" name="action" value="checkout" />
+					<input type="hidden" name="posData" value="" />
+					<input type="hidden" name="price" value="<?php echo $sum;?>" />
+					<input type="hidden" name="data" value="gKARxl3aXk+XK4ebKD2NeCzVODQZETKfLsZcHDtlADhaLM3br+dkzveLlDJdGC0marp9lyhuBmZGFQESte1b/9R/LyMXktY2vg9DxezTXE7NcNlr32DomZYURUD/FQqkNI5Ejhfn+L7Gi1wddJSMM9TTzv0g/nSvHzBupOgYWn8OZxPZ1jRP/bexS06mkqAijWuHC8Kpl4qaO9V9aaXIUYcsRsaTMFC4vcWou3U4rwIBLWTQfxkUHpJlyKLHTBVk" />
+					<input type="image" src="https://test.bitpay.com/img/button-large.png" border="0" name="submit" alt="BitPay, the easy way to pay with bitcoins." >
+				 </form>
 				
 				
 			</div>
